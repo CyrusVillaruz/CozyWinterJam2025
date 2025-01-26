7ae171602bb2843d7ad002f8c5ede9693199d59d
Elijah_Fabon <efabon2@my.bcit.ca>
2025-01-25 23:56:25 -0800

    Add player controller logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -95,6 +95,19 @@ public class PlayerController : MonoBehaviour
         wolvesSlainText.text = "Wolves slain: " + numWolvesSlain + "/" + totalWolves;
     }
 
+    private void OnClick()
+    {
+        StartCoroutine(Slash());
+    }
+
+    private IEnumerator Slash()
+    {
+        shootingPoint.GetComponent<Sword>().attacking = true;
+        yield return new WaitForSeconds(0.3f);
+        Debug.Log("Attack");
+        shootingPoint.GetComponent<Sword>().attacking = false;
+    }
+
     private void FixedUpdate()
     {
         rigidBody2D.linearVelocity = movementInput * acceleration;
